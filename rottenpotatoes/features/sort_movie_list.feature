1848add15ec9ac86d5ac10d7bd4778052176b1a3
EC2 Default User <ec2-user@ip-172-31-91-137.ec2.internal>
2021-03-05 02:50:34 +0000

    Part 1 complete

--- rottenpotatoes/features/sort_movie_list.feature
+++ rottenpotatoes/features/sort_movie_list.feature
@@ -24,8 +24,13 @@ Background: movies have been added to database
 
 Scenario: sort movies alphabetically
   When I follow "Movie Title"
-  # your steps here
+  Then I should be on the RottenPotatoes home page
+  And I should see "2001: A Space Odyssey" before "Aladdin"
+  And I should see "The Incredibles" before "The Terminator"
+  And I should see "Chocolat" before "Raiders of the Lost Ark"
 
 Scenario: sort movies in increasing order of release date
   When I follow "Release Date"
-  # your steps here
+  Then I should be on the RottenPotatoes home page
+  And I should see "1984" before "2004"
+  And I should see "Raiders of the Lost Ark" before "The Help"
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +18,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  (page.body.match? /#{e1}.*#{e2}/m).should be true
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +29,24 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  ratings = rating_list.split(", ")
+  if uncheck
+    ratings.each do |rating|
+      steps %Q{
+        When I uncheck "ratings_#{rating}"
+      }
+    end
+  else
+    ratings.each do |rating|
+      steps %Q{
+        When I check "ratings_#{rating}"
+      }
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  # counting all the rows in the table will include the header row, so we subtract 1 before comparing to the number of movies
+  (page.all(:element, "tr").count - 1).should be Movie.count
 end
